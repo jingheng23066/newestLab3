5482cc7cb1bfeaff694ccb52d74d7a4618b28b21
jingheng <jingheng.23@ichat.sp.edu.sg>
2025-05-12 11:14:46 +0800

    Updated test_Lab3.py

--- test_Lab3.py
+++ test_Lab3.py
@@ -1,7 +1,6 @@
+# Test_Lab3.py
+
 import Lab3
-
-print("Test_Lab3")
-
 
 def test_bubble_sort_ascending():
     result = []
@@ -21,10 +20,17 @@ def test_bubble_sort_descending():
 
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+def test_bubble_sort_greater_equal_10():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 1, 2, 3, 4]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert result == 1
 
-    result = Lab3.bubble_sort(input_arr, 3)
+def test_bubble_sort_zero_numbers():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert result == 0
 
-    assert (result == [])
+def test_bubble_sort_non_integer_input():
+    input_arr = [64, 34, "a", 12, 22, 11, 90]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert result == 2
