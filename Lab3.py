3d99fcd49553b1c3e8d1abbe0d1261e1b5016e02
jingheng <jingheng.23@ichat.sp.edu.sg>
2025-05-12 11:06:51 +0800

    finished Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,5 +1,7 @@
 print("Lab 3 - Software Unit Testing with PyTest")
 
+# Lab3.py
+
 SORT_ASCENDING = 0
 SORT_DESCENDING = 1
 
@@ -12,29 +14,33 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
-        # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
+    if n == 0:
+        return 0  # Return 0 for empty input array
 
-            # Last i elements are already in place
-            for j in range(0, n - i - 1):
+    if all(isinstance(x, int) for x in arr_result):  # Check if all elements are integers
+        if n < 10:
+            # Traverse through all array elements
+            for i in range(n - 1):
+                # range(n) also work but outer loop will
+                # repeat one time more than needed.
 
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                # Last i elements are already in place
+                for j in range(0, n - i - 1):
+
+                    if sorting_order == SORT_ASCENDING:
+                        if arr_result[j] > arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    elif sorting_order == SORT_DESCENDING:
+                        if arr_result[j] < arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
-                else:
-                    # Return an empty array
-                    arr_result = []
+        elif n >= 10:
+            arr_result = 1
+
     else:
-        arr_result = -1
+        return 2  # Return 2 for non-integer input
 
     return arr_result
 
